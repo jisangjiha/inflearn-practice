c0c2280bf6447734b77ec65097c01ceb68dfdb5a
Jisang Hong <jisang1024@gmail.com>
2022-11-08 22:08:05 +0900

    ConfirmButton to function

--- src/chapter08/ConfirmButton.jsx
+++ src/chapter08/ConfirmButton.jsx
@@ -1,25 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ConfirmButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isConfirmed: false };
-    this.handleConfirm = this.handleConfirm.bind(this);
-  }
+function ConfirmButton(props) {
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const handleConfirm = () => {
+    setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
+  };
 
-  handleConfirm() {
-    this.setState((prevState) => ({
-      isConfirmed: !prevState.isConfirmed,
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleConfirm} disabled={this.state.isConfirmed}>
-        {this.state.isConfirmed ? "확인됨" : "확인하기"}
-      </button>
-    );
-  }
+  return (
+    <button onClick={handleConfirm} disabled={isConfirmed}>
+      {isConfirmed ? "확인됨" : "확인하기"}
+    </button>
+  );
 }
 
 export default ConfirmButton;
