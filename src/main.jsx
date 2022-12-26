5ef83da338b6563ab1daad77877376066b456037
Jisang Hong <jisang1024@gmail.com>
2022-12-26 23:16:35 +0900

    chang the order in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,20 @@ import Library from "./chapter03/Library";
 import Clock from "./chapter04/Clock";
 import CommentList from "./chapter05/CommentList";
 import NotificationList from "./chapter06/NotificationList"; //재풀이 필요
+import Accommodate from "./chapter07/Accommodate";
 import ConfirmButton from "./chapter08/ConfirmButton";
 import AttendanceBook from "./chapter10/AttendanceBook";
-import Blocks from "./chapter15/Blocks";
 import DarkOrLight from "./chapter14/DarkOrLight"; //재풀이 필요
-import Accommodate from "./chapter07/Accommodate";
+import Blocks from "./chapter15/Blocks";
 
 //setInterval(() => {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Accommodate />
-    <DarkOrLight />
     <Blocks />
+    <DarkOrLight />
     <AttendanceBook />
     <ConfirmButton />
+    <Accommodate />
     <NotificationList />
     <CommentList />
     <Clock />
